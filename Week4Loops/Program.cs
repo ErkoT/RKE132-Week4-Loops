a90567441a88d55540d807a577eb6f67ef9fda06
Erko Targamaa <erkotargamaa@gmail.com>
2022-09-25 17:54:33 +0300

    week4

---
+++ Week4Loops/Program.cs
@@ -0,0 +1,9 @@
+﻿//FOR - loop
+for(int i = 0; i < 5; i++)
+{
+    Console.WriteLine("We dont need no education.");
+}
+
+Console.WriteLine("Kõik");
+
+//Console.WriteLine("We dont need no education.");
---
+++ WhileLoops/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Net.WebSockets;
+
+Random rnd = new Random();
+int cpuRandom;
+
+bool loopActive = true;
+
+while (loopActive)
+{
+    cpuRandom = rnd.Next(1, 4);
+    Console.WriteLine("Arva number 1-3 vahemikus");
+    int userNumber = Int32.Parse(Console.ReadLine());
+
+    if (userNumber == cpuRandom)
+    {
+        Console.WriteLine("Võitsid");
+        loopActive = false;
+    }
+    else
+    {
+        Console.WriteLine("Proovi uuesti");
+    }
+}
+Console.WriteLine("Tsau");
